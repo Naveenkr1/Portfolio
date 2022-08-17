e642b0625e04cae41eb049435b505454818cb5e0
Naveen Kumar <dafuqbc11@gmail.com>
2022-08-18 00:32:24 +0530

    resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="naveenkr.me" className="resume-link">
+            <a href="/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
